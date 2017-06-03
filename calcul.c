75bfb4f0c111223f43358d5742fb688342b6d4e9
NCC06 <gabriel.bonneteau@gmail.com>
2017-06-03 09:31:07 +0200

    pointeur sur fonctions marche!

--- calcul.c
+++ calcul.c
@@ -6,7 +6,6 @@ int multiplication(int n1,int n2);
 int soustraction (int n1, int n2);
 int division(int n1, int n2);
 
-
 int addition(int n1, int n2)
 {
 	return n1 + n2;
@@ -27,19 +26,16 @@ int division(int n1, int n2)
 	return n1 / n2;
 }
 
-int (* listeFonction[4])(int, int) = { addition, soustraction, multiplication, division }; 
-
-
-
 int main(int argc, char const *argv[])
 {
 	int choix = 0;
 	int nb1 = 0;
 	int nb2 = 0;
 	int resultat = 0;
+	int (*fonctionCalcul[4])(int,int) = {addition,soustraction,multiplication,division};
 
 	printf("CALCULATRICE\n");
-	printf("Choisissez un type de calcul\n1 pour une addition,\n2 pour une multiplication,\n3 pour une soustraction,\n4 pour une division\n");
+	printf("Choisissez un type de calcul\n1 pour une addition,\n2 pour une soustraction,\n3 pour une multiplication,\n4 pour une division\n");
 	scanf("%d", &choix);
 	
 	printf("Entrez les deux chiffres a calculer:\n Chiffre 1: ");
@@ -47,9 +43,9 @@ int main(int argc, char const *argv[])
 	printf("\nChiffre 2 : ");
 	scanf("%d", &nb2);	
 
-	resultat = (* listeFonction[choix - 1])(nb1, nb2);
 
-	printf("Resultat: ‰d", resultat);
+
+	printf("Resultat: %d\n", (*fonctionCalcul[choix-1])(nb1,nb2));
 
 	return EXIT_SUCCESS;			
 }
